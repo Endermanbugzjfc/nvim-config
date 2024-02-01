4da074d6e442350cd7ad3274f6d2347565ad4e68
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2024-02-01 19:21:27 +0800

    statusline: added file format (line break type) and removed unneccessary modules.

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -14,6 +14,11 @@ M.ui = {
   statusline = {
     theme = "vscode",
     overriden_modules = function (modules)
+      modules[10] = "" -- Encoding
+      modules[11] = "" -- Syntax
+      modules[12] = "" -- LSP
+      modules[13] = "" -- Folder
+
       -- https://github.com/NvChad/ui/blob/v2.0/lua/nvchad/statusline/vscode.lua
       local mode = ({
         ["NORMAL"] = "躺平",
@@ -28,6 +33,18 @@ M.ui = {
       })[string.match(modules[1], "%S+%s+%S+%s+(%S+)")]
       if mode ~= nil then
         modules[1] = "%#ST_Mode# " .. mode .. " "
+      end
+      
+      local buffer = vim.api.nvim_get_current_buf()
+      local format = vim.api.nvim_buf_get_option(buffer, "fileformat")
+      if format == "unix" then
+        modules[13] = "LF"
+      elseif format == "dos" then
+        modules[13] = "CRLF"
+      elseif format == "mac" then
+        modules[13] = "CR"
+      else
+        modules[13] = "NMSL"
       end
     end,
   },
