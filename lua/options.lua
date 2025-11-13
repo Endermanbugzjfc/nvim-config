ca4e196d855dc1238778f815a49ba55bce95cb99
Endermanbugzjfc <endermanbugzjfc@gmail.com>
2025-11-14 00:43:46 +1100

    Feat(auto cwd): change current directory to command line argument when provided.
    StackExchange answer log: https://vi.stackexchange.com/a/48364/59415

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,10 @@ vim.o.clipboard = ""
 
 -- https://tech.serhatteker.com/post/2020-06/close-all-buffers-but-current-in-vim/
 vim.cmd("command! Bonly :normal! mx :%bdelete|edit#|bdelete#<CR>")
+
+
+-- Automatically `cd` to last argument:
+vim.o.autochdir = true
+vim.schedule(function()
+  vim.o.autochdir = false
+end)
