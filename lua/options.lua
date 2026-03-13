3c00d272d2f0d1a4e5e2f15c37e44a74db670230
EndermanbugZJFC <endermanbugzjfc@gmail.com>
2026-03-14 00:34:41 +1100

    Add(command): async write :A.

--- lua/options.lua
+++ lua/options.lua
@@ -14,3 +14,26 @@ vim.o.autochdir = true
 vim.schedule(function()
   vim.o.autochdir = false
 end)
+
+-- Async write for laggy environment like SSHFS:
+vim.api.nvim_create_user_command('A', function()
+  local filepath = vim.api.nvim_buf_get_name(0)
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  local content = table.concat(lines, '\n') .. '\n'
+
+  vim.bo.modified = false  -- optimistically mark clean
+
+  local job_id = vim.fn.jobstart({'tee', filepath}, {
+    on_exit = function(_, code)
+      if code ~= 0 then
+        vim.schedule(function()
+          vim.notify('Write failed: ' .. filepath, vim.log.levels.ERROR)
+          vim.bo.modified = true
+        end)
+      end
+    end,
+  })
+
+  vim.fn.chansend(job_id, content)
+  vim.fn.chanclose(job_id, 'stdin')
+end, {})
